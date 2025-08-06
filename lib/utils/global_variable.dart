25de8a372123120f8d58f0832b5effe128056710
ashutosh-0907 <aashutoshrai0907@gmail.com>
2025-08-06 17:26:06 +0530

    All files added

---
+++ lib/utils/global_variable.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const webScreenSize = 600;
+
+/*List<Widget> homeScreenItems = [
+  const FeedScreen(),
+  const SearchScreen(),
+  const AddPostScreen(),
+  const Text('notifications'),
+  ProfileScreen(
+    uid: FirebaseAuth.instance.currentUser!.uid,
+  ),
+];*/
