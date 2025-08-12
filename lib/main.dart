5791168629c5a63f7f4110fba8f261c28dedb347
ashutosh-0907 <aashutoshrai0907@gmail.com>
2025-08-12 07:38:52 +0530

    "Project complete"

--- lib/main.dart
+++ lib/main.dart
@@ -38,7 +38,7 @@ class MyApp extends StatelessWidget {
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => UserProvider(),),
-      ],
+      ], 
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Instagram Clone',
